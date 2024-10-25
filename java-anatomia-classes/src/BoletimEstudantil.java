41a5a7bb93fb662112f403ee17c5eebadb31470e
Daniel Mayne Bento <danielmayne2021@gmail.com>
2024-10-24 21:29:51 -0300

    Commit de estrutura identada, mais facil visualização

--- java-anatomia-classes/src/BoletimEstudantil.java
+++ java-anatomia-classes/src/BoletimEstudantil.java
@@ -1,13 +1,13 @@
 public class BoletimEstudantil {
-public static void main(String [] args){
-int mediaFinal = 6;
-if(mediaFinal<6){
-System.out.println("reprovado");
-}else if (mediaFinal==6) {
-System.out.println("Prova Minerva");
-}else{
-System.out.println("Aprovado");
-}
-}
+    public static void main(String[] args) {
+        int mediaFinal = 6;
+        if (mediaFinal < 6) {
+            System.out.println("reprovado");
+        } else if (mediaFinal == 6) {
+            System.out.println("Prova Minerva");
+        } else {
+            System.out.println("Aprovado");
+        }
+    }
 
 }
