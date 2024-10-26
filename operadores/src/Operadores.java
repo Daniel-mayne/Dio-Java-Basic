65d7dfbaff7b03e228a489b22f2289113ad1fd99
Daniel Mayne Bento <danielmayne2021@gmail.com>
2024-10-26 09:14:51 -0300

    operador de  concatenação

---
+++ operadores/src/Operadores.java
@@ -0,0 +1,20 @@
+public class Operadores {
+    public static void main(String[] args) throws Exception {
+        String nomeCompleto = "Linguagem" + " Java";
+        System.out.println(nomeCompleto);
+
+
+        String concatenacao = "?";
+
+        concatenacao = 1+1+1+"1";
+        System.out.println(concatenacao);
+        concatenacao = 1+"1"+1+1;
+System.out.println(concatenacao);
+        concatenacao = 1+"1"+1+"1";
+System.out.println(concatenacao);
+        concatenacao = "1"+1+1+1;
+System.out.println(concatenacao);
+        concatenacao = "1"+(1+1+1);
+        System.out.println(concatenacao);
+    }
+}
