1cfd5476562878bc82c59e44096604383ac2d466
Daniel Mayne Bento <danielmayne2021@gmail.com>
2024-10-26 15:14:11 -0300

    conversão de valores

--- operadores/src/Operadores.java
+++ operadores/src/Operadores.java
@@ -1,20 +1,29 @@
 public class Operadores {
-    public static void main(String[] args) throws Exception {
-        String nomeCompleto = "Linguagem" + " Java";
-        System.out.println(nomeCompleto);
+        public static void main(String[] args) throws Exception {
+                // String nomeCompleto = "Linguagem" + " Java";
+                // System.out.println(nomeCompleto);
 
+                // String concatenacao = "?";
 
-        String concatenacao = "?";
+                // concatenacao = 1+1+1+"1";
+                // System.out.println(concatenacao);
+                // concatenacao = 1+"1"+1+1;
+                // System.out.println(concatenacao);
+                // concatenacao = 1+"1"+1+"1";
+                // System.out.println(concatenacao);
+                // concatenacao = "1"+1+1+1;
+                // System.out.println(concatenacao);
+                // concatenacao = "1"+(1+1+1);
+                // System.out.println(concatenacao);
 
-        concatenacao = 1+1+1+"1";
-        System.out.println(concatenacao);
-        concatenacao = 1+"1"+1+1;
-System.out.println(concatenacao);
-        concatenacao = 1+"1"+1+"1";
-System.out.println(concatenacao);
-        concatenacao = "1"+1+1+1;
-System.out.println(concatenacao);
-        concatenacao = "1"+(1+1+1);
-        System.out.println(concatenacao);
-    }
+                int numero = 5;
+                System.out.println( numero);
+
+                numero = - numero;
+                System.out.println(numero);
+
+                numero = numero * -1;
+
+                System.out.println(numero);
+        }
 }
