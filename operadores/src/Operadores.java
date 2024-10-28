636eb1dada535a86843662c7293ace669d799757
Daniel Mayne Bento <danielmayne2021@gmail.com>
2024-10-28 10:52:09 -0300

    Operadores de condição e comparação

--- operadores/src/Operadores.java
+++ operadores/src/Operadores.java
@@ -26,26 +26,65 @@ public class Operadores {
 
                 // System.out.println(numero);
 
-
                 // int numero = 10;
 
-                //  numero++;
+                // numero++;
                 // System.out.println(numero);
 
                 // int numero2 = 10;
                 // numero2--;
                 // System.out.println(numero2);
 
-                // boolean  verdadeiro = true;
+                // boolean verdadeiro = true;
 
                 // System.out.println(!verdadeiro);
 
-                int a,b;
+                // int a,b;
 
-                a = 5;
-                b = 6;
-                String resultado = a==b ? "Verdadeiro":"Falso";
-                System.out.println(resultado);
-                
+                // a = 5;
+                // b = 6;
+                // String resultado = a==b ? "Verdadeiro":"Falso";
+                // System.out.println(resultado);
+
+                // int numero1 = 1;
+
+                // int numero2 = 2;
+
+                // boolean simNao = numero1 == numero2;
+                // System.out.println(simNao);
+
+                // simNao = numero1 != numero2;
+
+                // System.out.println(simNao);
+
+                // simNao = numero1 > numero2;
+
+                // System.out.println(simNao);
+
+                // String nomeUm = "Daniel";
+                // String nomeDois = new String("Daniel");
+
+                // System.out.println(nomeUm == nomeDois);
+
+                // // para comparar objetos e string usar o metodo .equals
+
+                // System.out.println(nomeUm.equals(nomeDois));
+
+                // logicos
+
+                boolean condicao1 = true;
+
+                boolean condicao2 = false;
+
+                if (condicao1 && (7 > 4)) {
+                        System.out.println("As duas são verdadeiras");
+
+                }
+
+                if (condicao1 || condicao2) {
+                        System.out.println("Pelo menos uma das das condições é verdadeira");
+                }
+
+                System.out.println("Fim");
         }
 }
