6b941bf4f789bad785e3e5582b471724fead054f
Daniel Mayne Bento <danielmayne2021@gmail.com>
2024-10-28 17:21:19 -0300

    Limites de cada varivel e onde são acessivel

---
+++ Escopo/src/Escopo.java
@@ -0,0 +1,13 @@
+public class Escopo {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+
+        // No escopo, precisamos entender até onde cada variável é acessível. Temos as
+        // variáveis da classe,
+        // que são acessíveis em todos os métodos; também existem as variáveis de
+        // método, que são acessíveis
+        // apenas no próprio método, e as variáveis de bloco, que são acessíveis somente
+        // no bloco onde foram definidas.
+
+    }
+}
