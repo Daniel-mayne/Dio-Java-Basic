488d803e552112911066df339057ac291c0d64de
Daniel Mayne Bento <danielmayne2021@gmail.com>
2024-10-29 17:41:18 -0300

    Tags e comentarios

---
+++ Calculadora.java/src/Comentarios.java
@@ -0,0 +1,23 @@
+public class Comentarios {
+    public static void main(String[] args) {
+
+        // Olá, eu sou um comentario em uma única linha
+
+        /*
+         * Olá, eu sou um comentario
+         * em multiplas linhas se necessario
+         */
+
+    }
+
+    /**
+     * Esteas duas estrelinhas acima
+     * é para identificar que você
+     * pretende elaborar um comentário
+     * a nivel de documentação.
+     * Que incrivel !!!
+     */
+    public void metodo() {
+
+    }
+}
