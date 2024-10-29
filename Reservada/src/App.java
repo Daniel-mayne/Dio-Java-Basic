3fc6a33d457f69a6eea9e42bcc2e063f8b955dad
Daniel Mayne Bento <danielmayne2021@gmail.com>
2024-10-29 13:25:35 -0300

    palavras reservadas  JAVA

---
+++ Reservada/src/App.java
@@ -0,0 +1,103 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        // Palavras Reservadas Categorias
+
+        // Controle de pacote
+
+        // import: importa pacotes ou classes para dentro do código
+        // Package especifica a que pacote todas as classes de um arquivo pertencem
+
+        // Modificadores de acesso
+
+        // public: acesso de qualquer classes
+        // private: acesso apenas dentro da classes
+        // protected: acesso por classes no mesmo pacote e subclasses
+
+        // Primitivos
+
+        // boolean
+        // byte
+        // char
+        // double
+        // float
+        // int
+        // long
+        // short
+        // void
+
+        // Modificadores de classes, variaveis ou métodos
+        // abstract: classe que não pode ser instaciada ou método que precisa ser
+        // implementado por uma subclasse não abstrata
+
+        // class: especifica uma classe
+        // static: faz um método ou variável pertencer á classe ao invés de às
+        // instâncias
+
+        // extends: indica a superclasse que a succlasse esta estendendo
+
+        // final: impossibilita que uma classe seja estendida, que um metodo seja sobrescrito ou que  uma variavel seja reinicializada
+
+        // implements: indica as interfaces que uma classe ira implementar
+
+        // interface: especifica uma interface
+
+        // native: indica que um metodo esta escrito em uma linguagem dependente de plataforma, como o C
+
+        // new: instancia um novo objeto, chamando seu construtor
+
+        // static: faz um método ou variavel pertencer á classe ao inves de as instâncias
+
+        //  strictfp: usado em frente a um método ou classe para indicar que os numeros de pontos flutuantes seguiram  as regras de ponto flutuante em todas as expressões
+
+        // synchronized: indica que um método só pode ser acessado por uma thead de cada vez
+
+        // transient: impede a serialização de campos
+
+        // volatile: indica que uma variavel pode ser alterada durante o uso de threads
+
+
+
+        // Controle de fluxo dentro de um bloco de código
+         
+        // break: sai do bloco de codigo em que ele está
+
+        // case: executa um bloco de código dependendo do teste do switch 
+        
+        // continue: pula a execução do codigo que viria após essa linha e vai para a proxima passagem do loop 
+
+
+
+
+
+
+
+
+        // tratamentos de erros
+
+        // assert: testa uma expressão condicional para verificar uma suposição do programador
+    
+    
+        // catch: declara o bloco de codigo usado para tratar uma execução
+
+        // finally: bloco de codigo, após um try-catch, que é executado independentemente do fluxo de programa seguido ao lidar com uma exceção
+
+        // throw: usado para passar uma exceção para o método que chamou
+
+        // throws: indica que um método pode passar uma exceção para o metodo
+
+        // try: bloco de codigo que tentara ser executado, mas que pode causar uma exceção
+
+
+
+        // Variaveis de referencia
+
+        // super: refere-se a superclasse imediata
+
+        // this: refere-se a instancia atual do objeto
+
+    
+    
+    
+    }
+}
